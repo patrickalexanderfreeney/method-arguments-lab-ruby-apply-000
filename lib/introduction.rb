78b3567abef64c9d3951be4abbad4cc444120e06
Patrick Alexander <patrick_alexander@mail.com>
2019-08-10 04:34:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction  
-  
+def introduction(name) 
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and i'm learning to program in #{language} "
 end
